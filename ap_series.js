50bfb79d8b5783a4624e9d792b35539d53100728
PaulamiDutta23 <paulamidutta23@gmail.com>
2025-09-19 14:30:07 +0530

    Created this file to calculate the AP series for a given range

---
+++ ap_series.js
@@ -0,0 +1,8 @@
+const finalTerm = 5;
+let sum = 0;
+
+for (let currentTerm = 1; currentTerm <= finalTerm; currentTerm++) {
+    sum += currentTerm;
+}
+
+console.log("The result of AP series upto", finalTerm, "is", sum);
