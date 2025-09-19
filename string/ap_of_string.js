5c1e755d7df3369f5f6ff0d7c660e800e0c01255
PaulamiDutta23 <paulamidutta23@gmail.com>
2025-09-19 16:37:34 +0530

    Delete string directory

--- string/ap_of_string.js
+++
@@ -1,9 +0,0 @@
-const endRangeString = ".....";
-let resultString = "";
-let currentString = "";
-
-while(currentString <= endRangeString) {
-    resultString += currentString;
-    console.log(resultString, currentString, resultString.length);
-    currentString += ".";
-}
