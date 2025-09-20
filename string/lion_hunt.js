4ee24a4dde06a7a18986770b7485cffbb36c4e38
PaulamiDutta23 <paulamidutta23@gmail.com>
2025-09-20 16:39:46 +0530

    Updated to count the number of L, Z and Space in the test case string

--- string/lion_hunt.js
+++ string/lion_hunt.js
@@ -3,7 +3,24 @@ const testCase2 = "L L";
 const testCase3 = "L  ";
 const testCase4 = "  L ";
 const testCase5 = "    L";
-const testCaseToUse = testCase2;
+const testCaseToUse = testCase5;
 const testCaseLength = testCaseToUse.length;
+let countOfL = 0;
+let countOfZ = 0;
+let countOfSpace = 0;
 
+for(let index = 0; index < testCaseLength; index++) {
+    if(testCaseToUse[index] === "L") {
+        countOfL++;
+    }
+    else if(testCaseToUse[index] === "Z") {
+        countOfZ++;
+    }
+    else if(testCaseToUse[index] === " ") {
+        countOfSpace++;
+    }
+}
 console.log(testCaseLength);
+console.log(countOfL);
+console.log(countOfZ);
+console.log(countOfSpace);
