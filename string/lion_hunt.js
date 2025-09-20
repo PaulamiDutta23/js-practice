3d3bafadb5baaa41d5673c2300e664bf563da299
PaulamiDutta23 <paulamidutta23@gmail.com>
2025-09-20 16:32:54 +0530

    Created this file to find shortest distance between L and Z

---
+++ string/lion_hunt.js
@@ -0,0 +1,9 @@
+const testCase1 = "L";
+const testCase2 = "L L";
+const testCase3 = "L  ";
+const testCase4 = "  L ";
+const testCase5 = "    L";
+const testCaseToUse = testCase2;
+const testCaseLength = testCaseToUse.length;
+
+console.log(testCaseLength);
